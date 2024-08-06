42c1b594eff9034e7c93ca49e7e4091f82241387
Ivan Domrachev <domrachev03@mail.ru>
2024-08-06 23:14:05 +0300

    feat: fix position barrier, working constrained local inverse kinematics

--- examples/problem_solving/local_ik_constrained.py
+++ examples/problem_solving/local_ik_constrained.py
@@ -1,15 +1,16 @@
 import os
+import time
 
 import jax.numpy as jnp
 import mujoco as mj
 import mujoco.mjx as mjx
 import numpy as np
 from jaxlie import SE3, SO3
+from matplotlib import pyplot as plt
 from mujoco import viewer
 
 from mjinx import solve_local_ik
 from mjinx.barriers import ModelJointBarrier, PositionUpperBarrier
-from mjinx.configuration import update
 from mjinx.tasks import FrameTask
 
 model_path = os.path.abspath(os.path.dirname(__file__)) + "/../robot_descriptions/kuka_iiwa_14/iiwa14.xml"
@@ -24,6 +25,9 @@ mj_viewer = viewer.launch_passive(
     show_left_ui=False,
     show_right_ui=False,
 )
+q_min = mj_model.jnt_range[:, 0].copy()
+q_max = mj_model.jnt_range[:, 1].copy()
+mj_model.jnt_range = [(-20 * np.pi, 20 * np.pi) for _ in range(7)]
 renderer.scene.ngeom += 1
 mj_viewer.user_scn.ngeom = 1
 
@@ -59,10 +63,11 @@ barriers = {
     ),
     "position_barrier": PositionUpperBarrier(
         model=mjx_model,
-        gain=jnp.array([1e2, 1000.0, 1000.0]),
+        gain=jnp.array([100.0]),
         frame_id=ee_id,
-        p_max=np.array([0.3, 100.0, 100.0]),
-        safe_displacement_gain=1.0,
+        axes="x",
+        p_max=np.array([0.3]),
+        safe_displacement_gain=1e-2,
     ),
 }
 
@@ -78,42 +83,65 @@ mj.mjv_initGeom(
     np.array([0.565, 0.933, 0.565, 0.4]),
 )
 
-# try:
-# Warm-up JIT
-for t in ts:
-    tasks["ee_task"] = tasks["ee_task"].copy_and_set(
-        target_frame=SE3.from_rotation_and_translation(
-            SO3.identity(),
-            np.array(
-                [
-                    0.2 + 0.2 * jnp.sin(t) ** 2,
-                    0.2,
-                    0.2,
-                ]
-            ),
+try:
+    # Warm-up JIT
+    jnts = []
+    for t in ts:
+        tasks["ee_task"] = tasks["ee_task"].copy_and_set(
+            target_frame=SE3.from_rotation_and_translation(
+                SO3.identity(),
+                np.array(
+                    [
+                        0.2 + 0.2 * jnp.sin(t) ** 2,
+                        0.2,
+                        0.2,
+                    ]
+                ),
+            )
         )
-    )
-    vel = solve_local_ik(mjx_model, cur_q, tasks, barriers, damping=1e-12)
-    if vel is None:
-        raise ValueError("No solution found for IK")
-    cur_q += vel * dt
-    mj_data.qpos = cur_q
-    mj_data.qvel = vel
-    mj.mj_forward(mj_model, mj_data)
-    print(mj_data.xpos[ee_id][0])
-    mj.mjv_initGeom(
-        mj_viewer.user_scn.geoms[0],
-        mj.mjtGeom.mjGEOM_SPHERE,
-        0.1 * np.ones(3),
-        np.array(tasks["ee_task"].target_frame.translation(), dtype=np.float64),
-        np.eye(3).flatten(),
-        np.array([0.565, 0.933, 0.565, 0.4]),
-    )
+        t0 = time.perf_counter()
+        vel = solve_local_ik(mjx_model, cur_q, tasks, barriers, damping=1e-12)
+        print(f"Time: {(time.perf_counter() - t0)*1e3 :.3f} ms")
+        if vel is None:
+            raise ValueError("No solution found for IK")
 
-    # Run the forward dynamics to reflec
-    # the updated state in the data
-    mj.mj_forward(mj_model, mj_data)
-    mj_viewer.sync()
-# except Exception as e:
-#     print(e.with_traceback(None))
-#     mj_viewer.close()
+        cur_q += vel * dt
+        mj_data.qpos = cur_q
+        mj_data.qvel = vel
+        mj.mj_forward(mj_model, mj_data)
+        print(mj_data.xpos[ee_id][0])
+        mj.mjv_initGeom(
+            mj_viewer.user_scn.geoms[0],
+            mj.mjtGeom.mjGEOM_SPHERE,
+            0.1 * np.ones(3),
+            np.array(tasks["ee_task"].target_frame.translation(), dtype=np.float64),
+            np.eye(3).flatten(),
+            np.array([0.565, 0.933, 0.565, 0.4]),
+        )
+
+        # Run the forward dynamics to reflec
+        # the updated state in the data
+        mj.mj_forward(mj_model, mj_data)
+        mj_viewer.sync()
+
+        jnts.append(mj_data.qpos.copy())
+
+except Exception as e:
+    print(e.with_traceback(None))
+    mj_viewer.close()
+
+    jnts = np.array(jnts)
+
+    fig, ax = plt.subplots(7, 1, figsize=(10, 20))
+
+    for i in range(7):
+        ax[i].axhline(q_min[i], color="r", ls="--", label="Lower limit")
+        ax[i].axhline(q_max[i], color="r", ls="--", label="Upper limit")
+        ax[i].plot(ts, jnts[:, i], label=f"Joint {i}")
+        ax[i].set_title(f"Joint {i}")
+        ax[i].set_xlabel("Time (s)")
+        ax[i].set_ylabel("Joint position (rad)")
+        ax[i].legend()
+
+    plt.tight_layout()
+    plt.show()
