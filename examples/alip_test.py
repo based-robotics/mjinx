8e50fce3350ceb641e9b646ca9e7ee63c018b2d3
Ivan Domrachev <i_domrachev@kaist.ac.kr>
2025-03-09 23:50:41 +0900

    feat: proper alip implementation

--- examples/alip_test.py
+++ examples/alip_test.py
@@ -1,4 +1,6 @@
 import datetime
+from collections.abc import Callable
+from time import perf_counter
 
 import jax
 import jax.numpy as jnp
@@ -10,179 +12,189 @@ from matplotlib.animation import FFMpegFileWriter, FuncAnimation
 from robot_descriptions.cassie_mj_description import MJCF_PATH
 
 
-def dead_beat_alip(
-    model: mjx.Model,
-    q: jnp.ndarray,
-    v_des: jnp.ndarray,
-    com_height_des: jnp.ndarray,
+def make_dead_beat_alip(
     T_step: float,
     freq: int,
-    gait_width: float,
-    foot_height: float,
-    floor_height: float,
-    left_foot_id: float,
-    right_foot_id: float,
+    left_foot_id: int,
+    right_foot_id: int,
     n_steps: int,
-) -> tuple[jnp.ndarray, jnp.ndarray, jnp.ndarray]:
+) -> Callable[
+    [
+        mjx.Model,
+        mjx.Data,
+        jnp.ndarray,
+        jnp.ndarray,
+        float,
+        float,
+        float,
+    ],
+    tuple[jnp.ndarray, jnp.ndarray, jnp.ndarray],
+]:
     ticks_per_step = int(T_step * freq)
     g = 9.81
-    l = jnp.sqrt(g / com_height_des)
-    m = model.body_subtreemass[model.body_rootid[0]]
-    mH = m * com_height_des
-    A_alip = jnp.array(
-        [
-            [0, 0, 0, 1 / mH],
-            [0, 0, 1 / mH, 0],
-            [0, m * g, 0, 0],
-            [m * g, 0, 0, 0],
-        ]
-    )
-    A_alip_lin = jnp.eye(4) + A_alip / freq
-
-    def yaw_to_R(yaw: jnp.ndarray) -> jnp.ndarray:
-        return jnp.array(
-            [
-                [jnp.cos(yaw), -jnp.sin(yaw)],
-                [jnp.sin(yaw), jnp.cos(yaw)],
-            ]
-        )
 
     @jax.jit
-    def rollout_step(carry, v_des):
-        # Carry is:
-        # com_world: CoM position in world frame (x, y, yaw)
-        # L_init: CoM angular momentum
-        # p_stance: stance foot position in world frame, (x, y, z, yaw)
-        # p_swing: swing foot position in world frame (x, y, z, yaw)
-        # step_sign: -1 if left foot is stance, 1 if right foot is stance
-        com_init_world, L_init, p_stance_world, p_swing_world, step_sign = carry
-        vx_des, vy_des, wz_des = v_des
+    def dead_beat_alip(
+        mjx_model: mjx.Model,
+        mjx_data: mjx.Data,
+        com_height_des: jnp.ndarray,
+        v_des: jnp.ndarray,
+        gait_width: float,
+        foot_height: float,
+        floor_height: float,
+    ) -> tuple[jnp.ndarray, jnp.ndarray, jnp.ndarray]:
+        l = jnp.sqrt(g / com_height_des)
+        m = mjx_model.body_subtreemass[mjx_model.body_rootid[0]]
+        mH = m * com_height_des
 
-        p_stance_yaw = p_stance_world[3]
-        R_stance = jnp.array(
+        A_alip = jnp.array(
             [
-                [jnp.cos(p_stance_yaw), -jnp.sin(p_stance_yaw)],
-                [jnp.sin(p_stance_yaw), jnp.cos(p_stance_yaw)],
+                [0, 0, 0, 1 / mH],
+                [0, 0, 1 / mH, 0],
+                [0, m * g, 0, 0],
+                [m * g, 0, 0, 0],
             ]
         )
-        # Transform from world to local frames
-        p_stance2com = R_stance.T @ (com_init_world[:2] - p_stance_world[:2])
-        p_swing2com = R_stance.T @ (com_init_world[:2] - p_swing_world[:2])
-        x0 = jnp.concatenate([p_stance2com, L_init])
+        A_alip_lin = jnp.eye(4) + A_alip / freq
 
-        # Computing desired next stance foot position
-        # Formula 14
-        # L_minus = mH * l * jnp.sinh(l * T_step) * p_stance2com[::-1] + jnp.cosh(l * T_step) * L_init
-        sinh_lt, cosh_lt = jnp.sinh(l * T_step), jnp.cosh(l * T_step)
-        L_minus = mH * l * sinh_lt * p_stance2com[::-1] + cosh_lt * L_init
-        L_des = jnp.array(
-            [
-                # Formula 16
-                step_sign * mH * gait_width * l * sinh_lt / (2 * (1 + cosh_lt)) + mH * vy_des,
-                # From linear matrix
-                mH * vx_des,
-            ]
+        @jax.jit
+        def one_step_alip(carry, v_des):
+            # Carry is:
+            # com_world: CoM position in world frame (x, y, z, yaw)
+            # L_init: CoM angular momentum
+            # p_stance: stance foot position in world frame, (x, y, z, yaw)
+            # p_swing: swing foot position in world frame (x, y, z, yaw)
+            # step_sign: -1 if left foot is stance, 1 if right foot is stance
+            com_init_world, L_init, p_stance_world, p_swing_world, step_sign = carry
+            vx_des, vy_des, wz_des = v_des
+
+            p_stance_yaw = p_stance_world[3]
+            R_stance = jnp.array(
+                [
+                    [jnp.cos(p_stance_yaw), -jnp.sin(p_stance_yaw)],
+                    [jnp.sin(p_stance_yaw), jnp.cos(p_stance_yaw)],
+                ]
+            )
+            # Transform from world to local frames
+            p_stance2com = R_stance.T @ (com_init_world[:2] - p_stance_world[:2])
+            p_swing2com = R_stance.T @ (com_init_world[:2] - p_swing_world[:2])
+            x0 = jnp.concatenate([p_stance2com, L_init])
+
+            # Compute sinh and cosh once and for all
+            sinh_lt, cosh_lt = jnp.sinh(l * T_step), jnp.cosh(l * T_step)
+
+            # === Dead Beat ALIP
+            # Computing desired next stance foot position
+            # Formula 14
+            # L_minus = mH * l * jnp.sinh(l * T_step) * p_stance2com[::-1] + jnp.cosh(l * T_step) * L_init
+            L_minus = mH * l * sinh_lt * p_stance2com[::-1] + cosh_lt * L_init
+            L_des = jnp.array(
+                [
+                    # Formula 16
+                    step_sign * mH * gait_width * l * sinh_lt / (2 * (1 + cosh_lt)) + mH * vy_des,
+                    # From linear matrix
+                    mH * vx_des,
+                ]
+            )
+
+            # Formula 15
+            p_des = ((L_des - cosh_lt * L_minus) / (mH * l * sinh_lt))[::-1]
+
+            # === Generating step trajectory
+            s = jnp.linspace(0, 1, ticks_per_step)
+
+            # == CoM trajectory
+            # CoM trajectory relative to stance foot
+            _, x_traj = jax.lax.scan(
+                lambda x, _: (A_alip_lin @ x, A_alip_lin @ x),
+                x0,
+                None,
+                length=ticks_per_step,
+            )
+            # Yaw trajectory
+            _, com_yaw_traj = jax.lax.scan(
+                lambda yaw, _: (yaw + wz_des / freq, yaw + wz_des / freq),
+                com_init_world[3],
+                None,
+                length=ticks_per_step,
+            )
+            p_stance2com_traj, L_traj = x_traj[:, :2], x_traj[:, 2:]
+
+            # World frame
+            com_traj_world = jnp.hstack(
+                [
+                    (R_stance @ p_stance2com_traj.T).T + p_stance_world[:2],
+                    jnp.ones((ticks_per_step, 1)) * com_height_des,
+                    com_yaw_traj[:, None],
+                ]
+            )
+
+            # == Swing foot trajectory
+            # Relative to CoM
+            swing_foot_traj = jnp.array(
+                [
+                    ((1 + jnp.cos(jnp.pi * s)) * p_swing2com[0] + (1 - jnp.cos(jnp.pi * s)) * p_des[0]) / 2,
+                    ((1 + jnp.cos(jnp.pi * s)) * p_swing2com[1] + (1 - jnp.cos(jnp.pi * s)) * p_des[1]) / 2,
+                    foot_height - 4 * foot_height * (s - 0.5) ** 2 + floor_height,
+                ]
+            ).T
+            # Yaw trajectory
+            _, swing_foot_yaw_traj = jax.lax.scan(
+                lambda yaw, _: (yaw + 2 * wz_des / freq, yaw + 2 * wz_des / freq),
+                p_swing_world[3],
+                None,
+                length=ticks_per_step,
+            )
+            swing_foot_traj_world = jnp.hstack(
+                [
+                    com_traj_world[:, :2] - (R_stance @ swing_foot_traj[:, :2].T).T,
+                    swing_foot_traj[:, 2:3],
+                    swing_foot_yaw_traj[:, None],
+                ]
+            )
+
+            # == Stance foot trajectory
+            stance_foot_traj_world = jnp.tile(p_stance_world, (ticks_per_step, 1))
+
+            left_foot_traj = jax.lax.cond(
+                step_sign == 1, lambda: swing_foot_traj_world, lambda: stance_foot_traj_world
+            )
+            right_foot_traj = jax.lax.cond(
+                step_sign == 1, lambda: stance_foot_traj_world, lambda: swing_foot_traj_world
+            )
+
+            return (
+                com_traj_world[-1],
+                L_traj[-1],
+                swing_foot_traj_world[-1],
+                stance_foot_traj_world[-1],
+                -step_sign,
+            ), (
+                com_traj_world,
+                left_foot_traj,
+                right_foot_traj,
+            )
+
+        body_root = mjx_model.body_rootid[0]
+
+        # Initial parameters
+        com0 = jnp.concatenate([mjx_data.subtree_com[body_root, :2], com_height_des.reshape(1), jnp.zeros(1)])
+        L0 = mjx_data.subtree_angmom[body_root, :2]
+        # First step -- with right leg!
+        p_stance = jnp.concatenate([mjx_data.xpos[left_foot_id], jnp.zeros(1)])
+        p_swing = jnp.concatenate([mjx_data.xpos[right_foot_id], jnp.zeros(1)])
+        step_sign = -1
+
+        # Scan the step function to get a rollout with sequence of steps
+        _, (com_traj, left_foot_traj, right_foot_traj) = jax.lax.scan(
+            one_step_alip,
+            (com0, L0, p_stance, p_swing, step_sign),
+            v_des,
         )
 
-        # Formula 15
-        p_des = ((L_des - cosh_lt * L_minus) / (mH * l * sinh_lt))[::-1]
+        return com_traj, left_foot_traj, right_foot_traj
 
-        # === Generating step trajectory
-        s = jnp.linspace(0, 1, ticks_per_step)
-
-        # == CoM trajectory
-        # CoM trajectory relative to stance foot
-        _, x_traj = jax.lax.scan(
-            lambda x, _: (A_alip_lin @ x, A_alip_lin @ x),
-            x0,
-            None,
-            length=ticks_per_step,
-        )
-        # Yaw trajectory
-        _, com_yaw_traj = jax.lax.scan(
-            lambda yaw, _: (yaw + wz_des / freq, yaw + wz_des / freq),
-            com_init_world[2],
-            None,
-            length=ticks_per_step,
-        )
-        p_stance2com_traj, L_traj = x_traj[:, :2], x_traj[:, 2:]
-
-        # World frame
-        com_traj_world = jnp.hstack(
-            [
-                (R_stance @ p_stance2com_traj.T).T + p_stance_world[:2],
-                com_yaw_traj[:, None],
-            ]
-        )
-
-        # == Swing foot trajectory
-        # Relative to CoM
-        swing_foot_traj = jnp.array(
-            [
-                ((1 + jnp.cos(jnp.pi * s)) * p_swing2com[0] + (1 - jnp.cos(jnp.pi * s)) * p_des[0]) / 2,
-                ((1 + jnp.cos(jnp.pi * s)) * p_swing2com[1] + (1 - jnp.cos(jnp.pi * s)) * p_des[1]) / 2,
-                foot_height - 4 * foot_height * (s - 0.5) ** 2 + floor_height,
-            ]
-        ).T
-        # Yaw trajectory
-        _, swing_foot_yaw_traj = jax.lax.scan(
-            lambda yaw, _: (yaw + 2 * wz_des / freq, yaw + 2 * wz_des / freq),
-            p_swing_world[3],
-            None,
-            length=ticks_per_step,
-        )
-        swing_foot_traj_world = jnp.hstack(
-            [
-                com_traj_world[:, :2] - (R_stance @ swing_foot_traj[:, :2].T).T,
-                swing_foot_traj[:, 2:3],
-                swing_foot_yaw_traj[:, None],
-            ]
-        )
-
-        # == Stance foot trajectory
-        stance_foot_traj_world = jnp.tile(p_stance_world, (ticks_per_step, 1))
-
-        left_foot_traj = jax.lax.cond(step_sign == 1, lambda: swing_foot_traj_world, lambda: stance_foot_traj_world)
-        right_foot_traj = jax.lax.cond(step_sign == 1, lambda: stance_foot_traj_world, lambda: swing_foot_traj_world)
-        return (
-            com_traj_world[-1],
-            L_traj[-1],
-            swing_foot_traj_world[-1],
-            stance_foot_traj_world[-1],
-            -step_sign,
-        ), (
-            com_traj_world,
-            left_foot_traj,
-            right_foot_traj,
-        )
-
-    mjx_data: mjx.Data = mjx.make_data(model).replace(qpos=jnp.array(q0))
-    mjx_data = mjx.fwd_position(model, mjx_data)
-    body_root = model.body_rootid[0]
-
-    com0 = jnp.concatenate([mjx_data.subtree_com[body_root, :2], jnp.zeros(1)])
-    L0 = mjx_data.subtree_angmom[body_root, :2]
-    # First step -- with right leg!
-    p_stance = jnp.concatenate([mjx_data.xpos[left_foot_id], jnp.zeros(1)])
-    p_swing = jnp.concatenate([mjx_data.xpos[right_foot_id], jnp.zeros(1)])
-    step_sign = -1
-
-    _, (x_traj, left_foot_traj, right_foot_traj) = jax.lax.scan(
-        rollout_step,
-        (com0, L0, p_stance, p_swing, step_sign),
-        v_des * jnp.ones((n_steps, 3)),
-    )
-    # Adding constant height of CoM to the trajectory
-    com_traj = np.concatenate(
-        [
-            np.array(x_traj[:, :, :2]),
-            np.ones((n_steps, ticks_per_step, 1)) * com_height_des,
-            np.array(x_traj[:, :, 2:3]),
-        ],
-        axis=2,
-    )
-
-    return com_traj, np.array(left_foot_traj), np.array(right_foot_traj)
+    return dead_beat_alip
 
 
 def visualize_motion(
@@ -353,11 +365,6 @@ def visualize_motion(
 
 
 if __name__ == "__main__":
-    # Example: generate sample data
-    N = 200
-    t = np.linspace(0, 4 * np.pi, N)
-
-    # Sample trajectories -- replace these with your actual data.
     model = mj.MjModel.from_xml_path(MJCF_PATH)
     q0 = model.keyframe("home").qpos
     model = mjx.put_model(model)
@@ -370,18 +377,35 @@ if __name__ == "__main__":
     com0 = data.subtree_com[model.body_rootid[0]]
     floor_height = data.xpos[left_foot_id][2]
 
-    com_traj, left_foot_traj, right_foot_traj = dead_beat_alip(
-        model=model,
-        q=q0,
-        v_des=jnp.array([0.2, 0.0, 0.5]),
-        com_height_des=com0[2] - floor_height,
+    n_steps = 100
+    v_des = jnp.array([0.2, 0.0, 0.5]) * jnp.ones((n_steps, 3))
+    dead_beat_fn = make_dead_beat_alip(
         T_step=0.2,
         freq=100,
+        left_foot_id=left_foot_id,
+        right_foot_id=right_foot_id,
+        n_steps=100,
+    )
+    t0 = perf_counter()
+    com_traj, left_foot_traj, right_foot_traj = dead_beat_fn(
+        mjx_model=model,
+        mjx_data=data,
+        v_des=v_des,
+        com_height_des=com0[2] - floor_height,
         gait_width=data.xpos[right_foot_id][1] - data.xpos[left_foot_id][1],
         foot_height=0.03,
         floor_height=floor_height,
-        left_foot_id=left_foot_id,
-        right_foot_id=right_foot_id,
-        n_steps=20,
     )
+    t1 = perf_counter()
+    print("ALIP compilation time:", (t1 - t0) * 1e3, "ms")
+    com_traj, left_foot_traj, right_foot_traj = dead_beat_fn(
+        mjx_model=model,
+        mjx_data=data,
+        v_des=v_des,
+        com_height_des=com0[2] - floor_height,
+        gait_width=data.xpos[right_foot_id][1] - data.xpos[left_foot_id][1],
+        foot_height=0.03,
+        floor_height=floor_height,
+    )
+    print("ALIP execution time:", (perf_counter() - t1) * 1e3, "ms")
     visualize_motion(com_traj, left_foot_traj, right_foot_traj, freq=100)
