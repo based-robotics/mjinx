753a39c00cac52ac0a13a8a90d426c483d25fe05
Ivan Domrachev <i_domrachev@kaist.ac.kr>
2025-03-13 12:57:51 +0900

    chore: udpate disclamer for collision file

--- mjinx/configuration/_collision.py
+++ mjinx/configuration/_collision.py
@@ -1,17 +1,5 @@
-# Disclaimer: This function is adapted from MuJoCo mjx and is under their copyright:
-# Copyright 2023 DeepMind Technologies Limited
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# Disclaimer: This function is adapted from MuJoCo mjx
+# Link: https://github.com/google-deepmind/mujoco/blob/main/mjx/mujoco/mjx/_src/collision_driver.py
 
 import jax
 import jax.numpy as jnp
